5ca29cc1e3ba468be7c1c5d8931bae74c1c4a257
박주성 <121250776+Parkjuseong319@users.noreply.github.com>
2025-03-31 16:07:03 +0900

    [Bronze III] Title: 알람 시계, Time: 188 ms, Memory: 18516 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,24 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-
-        int h = sc.nextInt();
-        int m = sc.nextInt();
-        
-        if (m < 45) {
-            h = h - 1;
-            m = m + 60;
-            if (h < 0) {
-                h = h + 24;
-            }
-        }
-        System.out.print(h + " ");
-        System.out.println(m-45);
-        
-
-        sc.close();
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		if(m < 45) {
+			h = h - 1;
+			m += 60;
+			if(h < 0) h += 24;
+		}
+		System.out.print(h + " " + (m-45));
+		sc.close();
+	}
 }
