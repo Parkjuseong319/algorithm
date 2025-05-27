22c6fd0640bc47739346750dd87c6b8f8583e11b
박주성 <121250776+Parkjuseong319@users.noreply.github.com>
2025-05-27 18:05:13 +0900

    [Silver V] Title: 덩치, Time: 240 ms, Memory: 18356 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n][2];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i][0] = sc.nextInt();	// weight
+			arr[i][1] = sc.nextInt();	// height
+		}
+		
+		int x = n;
+		int y = n;
+		int rank = 1;
+		int[] arr2 = new int[n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+					rank++;
+				}else if((arr[i][0] > arr[j][0] && arr[i][0] < arr[j][0]) || (arr[i][0] < arr[j][0] && arr[i][1] > arr[j][1])) {
+					continue;
+				}
+			}
+			arr2[i] = rank;
+			rank = 1;
+		}
+		for (int i = 0; i < arr2.length; i++) {
+			System.out.print(arr2[i] + " ");
+		}
+		sc.close();
+	}
+}
