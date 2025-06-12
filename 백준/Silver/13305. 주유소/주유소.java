ebcd7b0da51045bcd4ad5075b8eafa9854b58483
박주성 <121250776+Parkjuseong319@users.noreply.github.com>
2025-06-12 14:48:03 +0900

    [Silver III] Title: 주유소, Time: 328 ms, Memory: 38852 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,13 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] distance = new int[n-1];
+		long[] distance = new long[n-1];
 		int[] oil = new int[n];
 		String[] d = br.readLine().split(" ");
 		String[] o = br.readLine().split(" ");
@@ -20,16 +19,14 @@ public class Main {
 		
 		// 연산. 가격은 long type으로 선언. 이진탐색문제..?
 		int minOil = Integer.MAX_VALUE;
-		int minIndex = 0;
 		long result = 0;
 		for (int i = 0; i < n-1; i++) {
 			if(oil[i] <= minOil) {
 				minOil = oil[i];
-				minIndex = i;
 			}
 			result += minOil * distance[i];
 		}
 		System.out.println(result);
-		br.close();	
+		br.close();
 	}
 }
