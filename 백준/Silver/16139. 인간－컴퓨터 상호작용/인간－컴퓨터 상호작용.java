fa1071844b56ffdcde0deacea8758cb202a06d00
박주성 <121250776+Parkjuseong319@users.noreply.github.com>
2026-02-26 16:16:58 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 820 ms, Memory: 117228 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -9,27 +9,28 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = null;
 
-        // 문자열 누적합
-        String[] str = new String[context.length() + 1];
-        for(int i = 0; i < context.length() + 1; i++){
-            str[i] = context.substring(0, i);
+        // 문자열 누적합. 2차원 배열 활용
+        int[][] arr = new int[context.length() + 1][26];
+        for(int i = 1; i <= context.length(); i++) {
+            //탐색 중인 문자
+            int searchChar = context.charAt(i-1)-'a';
+
+            //알파벳 a부터 z까지 반복
+            for(int j = 0; j < 26; j++) {
+                //현재 탐색중인 문자보다 한단계 앞인 문자의 값(=이전 값)
+                int beforeValue = arr[i-1][j];
+                //알파벳과 탐색 중인 문자가 같으면 이전값 + 1,다르면 이전값으로 넣음
+                arr[i][j] = ( j == searchChar ? beforeValue+1 : beforeValue);
+            }
         }
 
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             char a = st.nextToken().charAt(0);
             int l = Integer.parseInt(st.nextToken());
-            int r = Integer.parseInt(st.nextToken());
-            int res = 0;
+            int r = Integer.parseInt(st.nextToken()) + 1;
 
-            String s = str[r + 1].substring(l, r + 1);
-            char[] arr = s.toCharArray();
-            // 단순 계산. time over.
-            for (int j = 0; j < arr.length; j++) {
-                if(a == arr[j]) res++;
-            }
-
-            bw.write(res + "\n");
+            bw.write(arr[r][a - 'a'] - arr[l][a - 'a'] + "\n");
         }
         bw.flush();
         bw.close();
